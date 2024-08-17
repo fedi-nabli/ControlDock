a2bc37bf1cf0f494c0dbbeb104b346b44f874622
fedi-nabli <fedinabli@gmail.com>
2024-08-17 17:54:58 +0100

    Initial Commit

---
+++ ControlDock/ControlDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlDockApp.swift
+//  ControlDock
+//
+//  Created by Fedi Nabli on 17/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct ControlDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ControlDockTests/ControlDockTests.swift
@@ -0,0 +1,17 @@
+//
+//  ControlDockTests.swift
+//  ControlDockTests
+//
+//  Created by Fedi Nabli on 17/8/2024.
+//
+
+import Testing
+@testable import ControlDock
+
+struct ControlDockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
